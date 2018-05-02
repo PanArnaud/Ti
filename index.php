9c3ee3c58f53d24f7371737a1d3585005d5af03f
Arnaud Panapadeatchy <a.panapadeatchy@hotmail.fr>
2018-05-02 10:56:33 +0400

    Add Setter and Getter
    You can't update a PRIVATE attribute without Setter.

--- index.php
+++ index.php
@@ -5,9 +5,34 @@ class Personnage
   private $_force;
   private $_experience;
   private $_degats;
+  
+  // Getter, to access private attribute
+  public function getForce() {
+   return $this->_force; 
+  }
+
+  public function getExperience() {
+    return $this->_experience; 
+   }
+
+  public function getDegats() {
+    return $this->_degats; 
+  }
+
+  // Setter, to edit private attributes
+  public function setForce($force) {
+    $this->_force = $force;
+  }
+
+  public function setExperience($experience) {
+    $this->_experience = $experience;
+  }
+
+  public function setDegats($degats) {
+    $this->_degats = $degats;
+  }
 }
 
-$perso = new Personnage;
-$perso->_experience = $perso->_experience + 1; // Une erreur fatale est levée suite à cette instruction.
-
- ?>
+  $perso = new Personnage;
+  $perso->setDegats($perso->getDegats() + 1);
+?>
